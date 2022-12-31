e10bfccb54be2bcd06f8852e04bbbc4d760a6764
Chocolate <78719519+Chocolate996@users.noreply.github.com>
2022-12-31 16:09:16 +0800

    Add files via upload

--- page.js
+++ page.js
@@ -6,24 +6,31 @@ if (r18q == "no") {
   console.log("r18_yes");
 }
 
-var get_body_width = parseFloat(
-  getComputedStyle(document.querySelector("body")).width
-);
-console.log(get_body_width);
-
-let arr1 = document.getElementsByClassName("page-picture");
-
-if (get_body_width > "520") {
-  if (get_body_width > "1020") {
-    for (let i = 0, len = arr1.length; i < len; i++) {
-      document.getElementsByClassName("page-picture")[i].style.gridColumn =
-        "span 2";
-    }
-  } else {
-    for (let i = 0, len = arr1.length; i < len; i++) {
-      document.getElementsByClassName("page-picture")[i].style.gridColumn =
-        "span 3";
-    }
+if (/Android | webOS | iPhone /i.test(navigator.userAgent)) {
+  for (let i = 0, len = arr1.length; i < len; i++) {
+    document.getElementsByClassName("page-picture")[i].style.gridColumn =
+      "span 6";
   }
 } else {
+  var get_body_width = parseFloat(
+    getComputedStyle(document.querySelector("body")).width
+  );
+  console.log(get_body_width);
+
+  let arr1 = document.getElementsByClassName("page-picture");
+
+  if (get_body_width > "520") {
+    if (get_body_width > "1020") {
+      for (let i = 0, len = arr1.length; i < len; i++) {
+        document.getElementsByClassName("page-picture")[i].style.gridColumn =
+          "span 2";
+      }
+    } else {
+      for (let i = 0, len = arr1.length; i < len; i++) {
+        document.getElementsByClassName("page-picture")[i].style.gridColumn =
+          "span 3";
+      }
+    }
+  } else {
+  }
 }
