d088b0cb0c684034c3c42f6d8b9ddab15f2706a2
Chocolate <78719519+Chocolate996@users.noreply.github.com>
2023-01-01 12:57:12 +0800

    Add files via upload

--- page.js
+++ page.js
@@ -6,6 +6,8 @@ if (r18q == "no") {
   console.log("r18_yes");
 }
 
+  let arr1 = document.getElementsByClassName("page-picture");
+
 if (/Android | webOS | iPhone /i.test(navigator.userAgent)) {
   for (let i = 0, len = arr1.length; i < len; i++) {
     document.getElementsByClassName("page-picture")[i].style.gridColumn =
@@ -15,9 +17,6 @@ if (/Android | webOS | iPhone /i.test(navigator.userAgent)) {
   var get_body_width = parseFloat(
     getComputedStyle(document.querySelector("body")).width
   );
-  console.log(get_body_width);
-
-  let arr1 = document.getElementsByClassName("page-picture");
 
   if (get_body_width > "520") {
     if (get_body_width > "1020") {
@@ -34,3 +33,13 @@ if (/Android | webOS | iPhone /i.test(navigator.userAgent)) {
   } else {
   }
 }
+
+var turly_height =
+  parseFloat(
+    getComputedStyle(document.querySelectorAll(".page-grid")[0]).height
+  ) + "px";
+
+console.log(turly_height);
+
+document.documentElement.style.setProperty("--turly-height", turly_height);
+document.documentElement.style.setProperty("--before-height", "20vh");
