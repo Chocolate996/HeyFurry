3f9ef6571db71ea6afc2e69ddee533e35d4b6f9e
Chocolate <78719519+Chocolate996@users.noreply.github.com>
2023-01-08 16:27:07 +0800

    Add files via upload

--- Question/page.js
+++ Question/page.js
@@ -20,23 +20,36 @@ var f = 0;
 function ok_1() {
   if (f == 0) {
     document.getElementsByClassName("img")[0].style.backgroundImage =
-      "url(img/img2.jpg)";
+      "url(/img/img2.jpg)";
   }
   if (f == 1) {
     document.getElementsByClassName("img")[0].style.backgroundImage =
-      "url(img/img3.jpg)";
+      "url(/img/img3.jpg)";
   }
   if (f == 2) {
     document.getElementsByClassName("img")[0].style.backgroundImage =
-      "url(img/img4.jpg)";
+      "url(/img/img4.jpg)";
   }
   if (f == 3) {
     document.getElementsByClassName("img")[0].style.backgroundImage =
-      "url(img/img5.jpg)";
+      "url(/img/img5.jpg)";
   }
   if (f == 4) {
     window.location.href = "go.html";
     f = 0;
   }
   f++;
+}
+
+function goD() {
+  document.cookie = "go1=ok; expires=Thu, 18 Dec 2043 12:00:00 GMT";
+}
+function goO() {
+  document.cookie = "go2=ok; expires=Thu, 18 Dec 2043 12:00:00 GMT";
+}
+function goC() {
+  document.cookie = "go3=ok; expires=Thu, 18 Dec 2043 12:00:00 GMT";
+}
+function goG() {
+  document.cookie = "gog=no; expires=Thu, 18 Dec 2043 12:00:00 GMT";
 }
