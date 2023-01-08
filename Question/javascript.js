88ca221cb04cc2d733b80cfc1e3a64b5012da405
Chocolate <78719519+Chocolate996@users.noreply.github.com>
2023-01-08 12:50:32 +0800

    Add files via upload

---
+++ Question/javascript.js
@@ -0,0 +1,53 @@
+function question() {
+  document.cookie = "r18q=no; expires=Thu, 18 Dec 2043 12:00:00 GMT";
+}
+
+function getCookie(name) {
+  if (document.cookie.length > 0) {
+    start = document.cookie.indexOf(name + "=");
+    if (start != -1) {
+      start = start + name.length + 1;
+      end = document.cookie.indexOf(";", start);
+      if (end == -1) end = document.cookie.length;
+      return decodeURIComponent(document.cookie.substring(start, end));
+    }
+  }
+  return "";
+}
+
+if (getCookie("r18q") == "no") {
+  document.getElementById("nor18").className = "url-box-no";
+  document.getElementById("nor18").setAttribute("href", "#url");
+  document.getElementById("nor18").innerText = "抱歉，你未满18岁";
+} else {
+  console.log("r18_yes");
+  function r18() {
+    document.getElementById("nav").className = "nav-screen";
+    document.getElementsByClassName("nav-logo")[0].style.filter = "blur(10px)";
+    document.getElementsByClassName("nav-logo")[0].style.opacity = "0";
+    document.getElementsByClassName("r-18")[0].style.filter = "blur(0px)";
+    document.getElementsByClassName("r-18")[0].style.opacity = "1";
+  }
+}
+
+if (getCookie("r18can") == "no") {
+  document.getElementById("nor18").className = "url-box-no";
+  document.getElementById("nor18").setAttribute("href", "#url");
+  document.getElementById("nor18").innerText = "抱歉，你不能接受";
+} else {
+  console.log("r18can_yes");
+}
+
+//mark样式
+let observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("animate");
+      observer.unobserve(entry.target);
+    }
+  });
+});
+
+document.querySelectorAll("mark").forEach((mark) => {
+  observer.observe(mark);
+});
---
+++ Question/page.js
@@ -0,0 +1,42 @@
+if (getCookie("r18q") == "no") {
+  window.location.href = "index.html";
+} else {
+  console.log("r18_yes");
+}
+
+if (getCookie("r18can") == "no") {
+  window.location.href = "index.html";
+} else {
+  console.log("r18can_yes");
+}
+
+function r18can() {
+  document.cookie = "r18can=no; expires=Thu, 18 Dec 2043 12:00:00 GMT";
+  window.location.href = "index.html";
+}
+
+var a = ["1111", "22222", "3333", "44444"];
+var f = 0;
+function ok_1() {
+  if (f == 0) {
+    document.getElementsByClassName("img")[0].style.backgroundImage =
+      "url(/img/img2.jpg)";
+  }
+  if (f == 1) {
+    document.getElementsByClassName("img")[0].style.backgroundImage =
+      "url(/img/img3.jpg)";
+  }
+  if (f == 2) {
+    document.getElementsByClassName("img")[0].style.backgroundImage =
+      "url(/img/img4.jpg)";
+  }
+  if (f == 3) {
+    document.getElementsByClassName("img")[0].style.backgroundImage =
+      "url(/img/img5.jpg)";
+  }
+  if (f == 4) {
+    window.location.href = "go.html";
+    f = 0;
+  }
+  f++;
+}
