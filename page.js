67a58e340beb0fd838606f340e13ee1a34c2573c
Chocolate <78719519+Chocolate996@users.noreply.github.com>
2023-01-23 11:02:13 +0800

    Add files via upload

--- page.js
+++ page.js
@@ -5,9 +5,13 @@ if (r18q == "no") {
 } else {
   console.log("r18_yes");
 }
+var screen =
+  parseFloat(getComputedStyle(document.querySelector(".screen")).height) + "25";
+console.log(screen);
 
 let arr1 = document.getElementsByClassName("page-picture");
 let arr2 = document.getElementsByClassName("page-picture-w");
+let arr3 = document.getElementsByClassName("page-picture-q");
 
 if (/Android | webOS | iPhone /i.test(navigator.userAgent)) {
   for (let i = 0, len = arr1.length; i < len; i++) {
@@ -18,33 +22,44 @@ if (/Android | webOS | iPhone /i.test(navigator.userAgent)) {
     document.getElementsByClassName("page-picture-w")[i].style.gridColumn =
       "span 6";
   }
+  for (let i = 0, len = arr3.length; i < len; i++) {
+    document.getElementsByClassName("page-picture-q")[i].style.gridColumn =
+      "span 6";
+  }
 } else {
   var get_body_width = parseFloat(
     getComputedStyle(document.querySelector("body")).width
   );
+}
 
-  if (get_body_width > "500") {
-    if (get_body_width > "1020") {
-      for (let i = 0, len = arr1.length; i < len; i++) {
-        document.getElementsByClassName("page-picture")[i].style.gridColumn =
-          "span 2";
-      }
-    } else {
-      for (let i = 0, len = arr1.length; i < len; i++) {
-        document.getElementsByClassName("page-picture")[i].style.gridColumn =
-          "span 3";
-        for (let i = 0, len = arr2.length; i < len; i++) {
-          document.getElementsByClassName("page-picture-w")[
-            i
-          ].style.gridColumn = "span 6";
-        }
-      }
+if (get_body_width > "500") {
+  if (get_body_width > "1020") {
+    for (let i = 0, len = arr1.length; i < len; i++) {
+      document.getElementsByClassName("page-picture")[i].style.gridColumn =
+        "span 2";
     }
   } else {
+    for (let i = 0, len = arr1.length; i < len; i++) {
+      document.getElementsByClassName("page-picture")[i].style.gridColumn =
+        "span 3";
+    }
     for (let i = 0, len = arr2.length; i < len; i++) {
       document.getElementsByClassName("page-picture-w")[i].style.gridColumn =
         "span 6";
     }
+    for (let i = 0, len = arr3.length; i < len; i++) {
+      document.getElementsByClassName("page-picture-q")[i].style.gridColumn =
+        "span 6";
+    }
+  }
+} else {
+  for (let i = 0, len = arr2.length; i < len; i++) {
+    document.getElementsByClassName("page-picture-w")[i].style.gridColumn =
+      "span 6";
+  }
+  for (let i = 0, len = arr3.length; i < len; i++) {
+    document.getElementsByClassName("page-picture-q")[i].style.gridColumn =
+      "span 6";
   }
 }
 
@@ -52,21 +67,21 @@ var turly_height =
   parseFloat(
     getComputedStyle(document.querySelectorAll(".page-grid")[0]).height
   ) -
-  "100" +
+  screen +
   "px";
 
 var turly_height_1 =
   parseFloat(
     getComputedStyle(document.querySelectorAll(".page-grid")[1]).height
   ) -
-  "100" +
+  screen +
   "px";
 
 var turly_height_2 =
   parseFloat(
     getComputedStyle(document.querySelectorAll(".page-grid")[2]).height
   ) -
-  "100" +
+  screen +
   "px";
 
 console.log(turly_height);
